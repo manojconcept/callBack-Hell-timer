520be00d22785a63f5c429942d789cadf028049d
manojconcept <manojveerapavu@gmail.com>
2023-11-17 23:36:33 +0530

    fixed

--- src/script/script.js
+++ src/script/script.js
@@ -3,38 +3,38 @@ console.log("Linked")
 let getcbhT = document.getElementById("pointerOut");
 let getTitle = document.querySelector("title");
 setTimeout(() => {
-    getcbhT.innerText = "CallBack hell Every finshed 10 second's ";
+    getcbhT.innerText = "10";
     getTitle.innerText = "CBHT-10 "
     setTimeout(() => {
-        getcbhT.innerText = "CallBack hell Every finshed 9 second's "
+        getcbhT.innerText = "9"
         getTitle.innerText = "CBHT-9 "
 
         setTimeout(() => {
-            getcbhT.innerText = "CallBack hell Every finshed 8 second's"
+            getcbhT.innerText = "8"
             getTitle.innerText = "CBHT-8 "
 
             setTimeout(() => {
-                getcbhT.innerText = "CallBack hell Every finshed 7 second's"
+                getcbhT.innerText = "7"
                 getTitle.innerText = "CBHT-7 "
 
                 setTimeout(() => {
-                    getcbhT.innerText = "CallBack hell Every finshed 6 second's"
+                    getcbhT.innerText = "6"
                     getTitle.innerText = "CBHT-6 "
 
                     setTimeout(() => {
-                        getcbhT.innerText = "CallBack hell Every finshed 5 second's"
+                        getcbhT.innerText = "5"
                         getTitle.innerText = "CBHT-5 "
 
                         setTimeout(() => {
-                            getcbhT.innerText = "CallBack hell Every finshed 4 second's"
+                            getcbhT.innerText = "4"
                             getTitle.innerText = "CBHT-4 "
 
                             setTimeout(() => {
-                                getcbhT.innerText = "CallBack hell Every finshed 3 second's"
+                                getcbhT.innerText = "3"
                                 getTitle.innerText = "CBHT-3 "
 
                                 setTimeout(() => {
-                                    getcbhT.innerText = "CallBack hell Every finshed 2 second's"
+                                    getcbhT.innerText = "2"
                                     getTitle.innerText = "CBHT-2 "
 
                                     setTimeout(() => {
